05b9c900b1a7f104672f80d07bde6195d523e61d
Aaditya Vijay <aaditya.vijay25@gmail.com>
2026-08-30 22:06:05 +0530

    Add Factorial solution

--- Recursion/Fibo.java
+++ Recursion/Fibo.java
@@ -2,10 +2,8 @@ package Recursion;
 
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(fibo(6));
-        
+        System.out.println(fibo(6));   
     }
-
     static int fibo(int n){
         if (n<2){
             return n;

4b37e727071e562868db4914ae32bdb3b5de915f
Aaditya Vijay <aaditya.vijay25@gmail.com>
2026-09-07 21:54:09 +0530

    Add Number appearing  solution (Hashing)

---
+++ Hashing/Appears.java
@@ -0,0 +1,30 @@
+package Hashing;
+
+import java.util.Scanner;
+
+public class Appears {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i<=n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int hash[] = new int[13];
+        for(int i=0; i<=n; i++){
+            hash[arr[i]] += 1;
+        }
+
+        int q = sc.nextInt();
+        while(q>0){
+            int number = sc.nextInt();
+            if (number >= 0 && number < hash.length) {
+                System.out.println(hash[number]); 
+            } else {
+                System.out.println(0);
+            }
+            q--;
+        }
+    }
+    
+}
